f88e4d0f94beb7989d96f71ecb781f9ad1640442
Aleksandr <ZeroDay9898@gmail.com>
2022-12-03 15:52:47 +0300

    initial

---
+++ src/test/java/CreepTenuous/CreepTenuousApplicationTests.java
@@ -0,0 +1,13 @@
+package CreepTenuous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreepTenuousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
