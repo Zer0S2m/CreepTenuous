aead333c5bb4575af9c710d00cb2306cbf22307b
Aleksandr <ZeroDay9898@gmail.com>
2022-12-21 20:42:51 +0300

    set env in project

--- src/main/java/CreepTenuous/CreepTenuousApplication.java
+++ src/main/java/CreepTenuous/CreepTenuousApplication.java
@@ -2,6 +2,7 @@ package CreepTenuous;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class CreepTenuousApplication {
     public static void main(String[] args) {
---
+++ src/main/java/CreepTenuous/components/RootPath.java
@@ -0,0 +1,23 @@
+package CreepTenuous.components;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public final class RootPath {
+    private final String rootPath;
+
+    public RootPath(@Value("${ROOT_PATH}") String rootPath) {
+        if (!Objects.equals(rootPath.charAt(rootPath.length() - 1), "/")) {
+            this.rootPath = rootPath + "/";
+        } else {
+            this.rootPath = rootPath;
+        }
+    }
+
+    public String getRootPath() {
+        return this.rootPath;
+    }
+}
