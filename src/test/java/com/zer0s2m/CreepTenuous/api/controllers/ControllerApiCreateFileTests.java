cb65c257812fdfc17533fb43c0cce135e3a3d228
Aleksandr <ZeroDay9898@gmail.com>
2023-04-10 22:40:17 +0300

    Added tests for category: Create file - tests for controller and services

---
+++ src/test/java/com/zer0s2m/CreepTenuous/api/controllers/ControllerApiCreateFileTests.java
@@ -0,0 +1,147 @@
+package com.zer0s2m.CreepTenuous.api.controllers;
+
+import com.zer0s2m.CreepTenuous.api.controllers.files.create.data.DataCreateFile;
+import com.zer0s2m.CreepTenuous.services.directory.manager.enums.Directory;
+import com.zer0s2m.CreepTenuous.services.directory.manager.exceptions.messages.ExceptionNotDirectoryMsg;
+import com.zer0s2m.CreepTenuous.services.files.create.exceptions.messages.FileAlreadyExistsMsg;
+import com.zer0s2m.CreepTenuous.services.files.create.exceptions.messages.NotFoundTypeFileMsg;
+import com.zer0s2m.CreepTenuous.services.files.enums.ExceptionFile;
+import com.zer0s2m.CreepTenuous.services.files.enums.TypeFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class ControllerApiCreateFileTests {
+    Logger logger = LogManager.getLogger(ControllerApiCreateFileTests.class);
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    protected String nameTestFile1 = "testFile_1";
+    protected String nameTestFile2 = "testFile_2";
+    protected String nameTestFile3 = "testFile_3";
+    protected String nameTestFile4 = "testFile_4";
+
+    DataCreateFile RECORD_1 = new DataCreateFile(1, nameTestFile1, new ArrayList<>());
+    DataCreateFile RECORD_2 = new DataCreateFile(2, nameTestFile2, new ArrayList<>());
+    DataCreateFile RECORD_3 = new DataCreateFile(3, nameTestFile3, new ArrayList<>());
+    DataCreateFile INVALID_RECORD_TYPE_FILE = new DataCreateFile(9999, "failFile", new ArrayList<>());
+    DataCreateFile INVALID_RECORD_PATH_DIRECTORY = new DataCreateFile(
+            1,
+            "failFile",
+            Arrays.asList("invalid", "path", "directory")
+    );
+
+    @Test
+    public void createFile_success() throws Exception {
+        for (DataCreateFile record : Arrays.asList(RECORD_1, RECORD_2, RECORD_3)) {
+            Path pathTestFile = Path.of(
+                    System.getenv("ROOT_PATH")
+                            + Directory.SEPARATOR.get()
+                            + record.nameFile()
+                            + "."
+                            + TypeFile.getExtension(record.typeFile())
+            );
+            logger.info("Create file for tests: " + pathTestFile);
+
+            this.mockMvc.perform(
+                    MockMvcRequestBuilders.post("/api/v1/file/create")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(record))
+            )
+                    .andExpect(status().isCreated());
+            Assertions.assertTrue(Files.exists(pathTestFile));
+
+            logger.info(
+                    "Is deleted file for tests: " + Files.deleteIfExists(pathTestFile) + " (" + pathTestFile + ")"
+            );
+        }
+    }
+
+    @Test
+    public void createFile_fail_invalidTypeFile() throws Exception {
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/file/create")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(INVALID_RECORD_TYPE_FILE))
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json(
+                        objectMapper.writeValueAsString(
+                                new NotFoundTypeFileMsg(ExceptionFile.NOT_FOUND_TYPE_FILE.get())
+                        )
+                ));
+    }
+
+    @Test
+    public void createFile_fail_invalidPathDirectory() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/file/create")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(INVALID_RECORD_PATH_DIRECTORY))
+                )
+                .andExpect(status().isNotFound())
+                .andExpect(content().json(
+                        objectMapper.writeValueAsString(
+                                new ExceptionNotDirectoryMsg(Directory.NOT_FOUND_DIRECTORY.get())
+                        )
+                ));
+    }
+
+    @Test
+    public void createFile_fail_fileIsExists() throws Exception {
+        Path testFile = Files.createFile(Path.of(
+                    System.getenv("ROOT_PATH")
+                        + Directory.SEPARATOR.get()
+                        + nameTestFile4
+                        + "."
+                        + TypeFile.TXT.getExtension()
+        ));
+        logger.info("Created file for test: " + testFile);
+
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/file/create")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(
+                                        new DataCreateFile(1, nameTestFile4, new ArrayList<>())
+                                ))
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json(
+                        objectMapper.writeValueAsString(
+                                new FileAlreadyExistsMsg(ExceptionFile.FILE_ALREADY_EXISTS.get())
+                        )
+                ));
+
+        logger.info("Is deleted file for tests: " + Files.deleteIfExists(testFile) + " (" + testFile + ")");
+    }
+}
---
+++ src/test/java/com/zer0s2m/CreepTenuous/services/files/ServiceCreateFileTests.java
@@ -0,0 +1,171 @@
+package com.zer0s2m.CreepTenuous.services.files;
+
+import com.zer0s2m.CreepTenuous.api.controllers.files.create.data.DataCreateFile;
+import com.zer0s2m.CreepTenuous.components.RootPath;
+import com.zer0s2m.CreepTenuous.providers.build.os.services.impl.BuildDirectoryPath;
+import com.zer0s2m.CreepTenuous.services.common.collectRootPath.impl.CollectRootPath;
+import com.zer0s2m.CreepTenuous.services.directory.manager.enums.Directory;
+import com.zer0s2m.CreepTenuous.services.files.create.exceptions.NotFoundTypeFileException;
+import com.zer0s2m.CreepTenuous.services.files.create.services.impl.ServiceCreateFile;
+import com.zer0s2m.CreepTenuous.services.files.enums.TypeFile;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@SpringBootTest(classes = {
+        ServiceCreateFile.class,
+        BuildDirectoryPath.class,
+        CollectRootPath.class,
+        RootPath.class,
+})
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class ServiceCreateFileTests {
+    Logger logger = LogManager.getLogger(ServiceCreateFileTests.class);
+
+    @Autowired
+    private ServiceCreateFile service;
+
+    @Autowired
+    private BuildDirectoryPath buildDirectoryPath;
+
+    private final String nameTestFile1 = "tesFile_1";
+    private final String nameTestFile2 = "tesFile_2";
+    private final String nameTestFile3 = "tesFile_3";
+    private final String nameTestFile4 = "tesFile_4";
+
+    DataCreateFile RECORD_1 = new DataCreateFile(1, nameTestFile1, new ArrayList<>());
+    DataCreateFile RECORD_2 = new DataCreateFile(2, nameTestFile2, new ArrayList<>());
+    DataCreateFile RECORD_3 = new DataCreateFile(3, nameTestFile3, new ArrayList<>());
+    DataCreateFile INVALID_RECORD_FILE_EXISTS = new DataCreateFile(1, nameTestFile4, new ArrayList<>());
+    DataCreateFile INVALID_RECORD_TYPE_FILE = new DataCreateFile(9999, "failFile", new ArrayList<>());
+    DataCreateFile INVALID_RECORD_PATH_DIRECTORY = new DataCreateFile(
+            1,
+            "failFile",
+            Arrays.asList("fail", "path", "directory")
+    );
+
+    @Test
+    public void createFileType1_success() throws NotFoundTypeFileException, IOException {
+        final String nameFile = nameTestFile1 + "." + TypeFile.getExtension(RECORD_1.typeFile());
+        final Path pathTestFile = Paths.get(
+                buildDirectoryPath.build(RECORD_1.parents()) + Directory.SEPARATOR.get() + nameFile
+        );
+
+        logger.info("Create file for test: " + pathTestFile);
+
+        service.create(
+                RECORD_1.parents(),
+                RECORD_1.nameFile(),
+                RECORD_1.typeFile()
+        );
+
+        Assertions.assertTrue(Files.exists(pathTestFile));
+        logger.info("Is delete file for test " + "(" + pathTestFile + ") - " + Files.deleteIfExists(
+                pathTestFile
+        ));
+    }
+
+    @Test
+    public void createFileType2_success() throws NotFoundTypeFileException, IOException {
+        final String nameFile = nameTestFile2 + "." + TypeFile.getExtension(RECORD_2.typeFile());
+        final Path pathTestFile = Paths.get(
+                buildDirectoryPath.build(RECORD_2.parents()) + Directory.SEPARATOR.get() + nameFile
+        );
+        logger.info("Create file for test: " + pathTestFile);
+
+        service.create(
+                RECORD_2.parents(),
+                RECORD_2.nameFile(),
+                RECORD_2.typeFile()
+        );
+
+        Assertions.assertTrue(Files.exists(pathTestFile));
+        logger.info("Is delete file for test " + "(" + pathTestFile + ") - " + Files.deleteIfExists(
+                pathTestFile
+        ));
+    }
+
+    @Test
+    public void createFileType3_success() throws NotFoundTypeFileException, IOException {
+        final String nameFile = nameTestFile3 + "." + TypeFile.getExtension(RECORD_3.typeFile());
+        final Path pathTestFile = Paths.get(
+                buildDirectoryPath.build(RECORD_3.parents()) + Directory.SEPARATOR.get() + nameFile
+        );
+        logger.info("Create file for test: " + pathTestFile);
+
+        service.create(
+                RECORD_3.parents(),
+                RECORD_3.nameFile(),
+                RECORD_3.typeFile()
+        );
+
+        Assertions.assertTrue(Files.exists(pathTestFile));
+        logger.info("Is delete file for test " + "(" + pathTestFile + ") - " + Files.deleteIfExists(
+                pathTestFile
+        ));
+    }
+
+    @Test
+    public void createFile_fail_fileIsExists() throws IOException {
+        final String nameFile =
+                INVALID_RECORD_FILE_EXISTS.nameFile()
+                + "."
+                + TypeFile.getExtension(INVALID_RECORD_FILE_EXISTS.typeFile());
+        final Path pathTestFile = Paths.get(
+                buildDirectoryPath.build(
+                        INVALID_RECORD_FILE_EXISTS.parents()
+                ) + Directory.SEPARATOR.get() + nameFile
+        );
+        final Path newPathTestFile = Files.createFile(pathTestFile);
+        logger.info("Create file for test: " + newPathTestFile);
+
+        Assertions.assertThrows(
+                FileAlreadyExistsException.class,
+                () -> service.create(
+                        INVALID_RECORD_FILE_EXISTS.parents(),
+                        INVALID_RECORD_FILE_EXISTS.nameFile(),
+                        INVALID_RECORD_FILE_EXISTS.typeFile()
+                )
+        );
+
+        Assertions.assertTrue(Files.exists(newPathTestFile));
+        logger.info("Is delete file for test " + "(" + newPathTestFile + ") - " + Files.deleteIfExists(
+                pathTestFile
+        ));
+    }
+
+    @Test
+    public void createFile_fail_invalidTypeFile() {
+        Assertions.assertThrows(
+                NotFoundTypeFileException.class,
+                () -> service.create(
+                        INVALID_RECORD_TYPE_FILE.parents(),
+                        INVALID_RECORD_TYPE_FILE.nameFile(),
+                        INVALID_RECORD_TYPE_FILE.typeFile()
+                )
+        );
+    }
+
+    @Test
+    public void createFile_fail_invalidPathDirectory() {
+        Assertions.assertThrows(
+                NoSuchFileException.class,
+                () -> service.create(
+                        INVALID_RECORD_PATH_DIRECTORY.parents(),
+                        INVALID_RECORD_PATH_DIRECTORY.nameFile(),
+                        INVALID_RECORD_PATH_DIRECTORY.typeFile()
+                )
+        );
+    }
+}
