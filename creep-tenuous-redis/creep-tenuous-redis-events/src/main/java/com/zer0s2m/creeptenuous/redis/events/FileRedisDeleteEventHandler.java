eb71edef147a90e4869a108b6522794565f17565
Aleksandr <ZeroDay9898@gmail.com>
2023-06-18 00:36:51 +0300

    Removing user permissions when deleting a file from Redis

--- creep-tenuous-redis/creep-tenuous-redis-events/src/main/java/com/zer0s2m/creeptenuous/redis/events/FileRedisDeleteEventHandler.java
+++ creep-tenuous-redis/creep-tenuous-redis-events/src/main/java/com/zer0s2m/creeptenuous/redis/events/FileRedisDeleteEventHandler.java
@@ -1,9 +1,16 @@
 package com.zer0s2m.creeptenuous.redis.events;
 
 import com.zer0s2m.creeptenuous.common.enums.ManagerRights;
+import com.zer0s2m.creeptenuous.redis.models.RightUserFileSystemObjectRedis;
+import com.zer0s2m.creeptenuous.redis.repository.JwtRedisRepository;
+import com.zer0s2m.creeptenuous.redis.repository.RightUserFileSystemObjectRedisRepository;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Redis Delete Directory Object Handler
@@ -13,12 +20,57 @@ class FileRedisDeleteEventHandler implements ApplicationListener<FileRedisDelete
 
     static private final String SEPARATOR_UNIQUE_KEY = ManagerRights.SEPARATOR_UNIQUE_KEY.get();
 
+    private final JwtRedisRepository jwtRedisRepository;
+
+    private final RightUserFileSystemObjectRedisRepository rightUserFileSystemObjectRedisRepository;
+
+    @Autowired
+    FileRedisDeleteEventHandler(JwtRedisRepository jwtRedisRepository,
+                                RightUserFileSystemObjectRedisRepository rightUserFileSystemObjectRedisRepository) {
+        this.jwtRedisRepository = jwtRedisRepository;
+        this.rightUserFileSystemObjectRedisRepository = rightUserFileSystemObjectRedisRepository;
+    }
+
     /**
      * Handle an application event.
      * @param event the event to respond to
      */
     public void onApplicationEvent(@NotNull FileRedisDeleteEvent event) {
-        System.out.println(event.getIdFileRedis());
+        List<String> userLogins = getUserLogins();
+        Iterable<RightUserFileSystemObjectRedis> rightsUser = getRightUserFileSystemObjectRedis(
+                userLogins, event.getIdFileRedis());
+        deleteRightUserFileSystemObjectRedis(rightsUser);
+    }
+
+    /**
+     * Get all usernames in the system
+     * @return user logins
+     */
+    private @NotNull List<String> getUserLogins() {
+        List<String> userLogins = new ArrayList<>();
+        jwtRedisRepository.findAll().forEach(entity -> userLogins.add(entity.getLogin()));
+        return userLogins;
+    }
+
+    /**
+     * Get all user rights by user logins and the system name of the file system object
+     * @param userLogins user logins
+     * @param nameFileSystemObject system name of the file system object
+     * @return  user rights
+     */
+    private @NotNull Iterable<RightUserFileSystemObjectRedis> getRightUserFileSystemObjectRedis(
+            @NotNull List<String> userLogins, String nameFileSystemObject) {
+        List<String> ids = new ArrayList<>();
+        userLogins.forEach(userLogin -> ids.add(nameFileSystemObject + SEPARATOR_UNIQUE_KEY + userLogin));
+        return rightUserFileSystemObjectRedisRepository.findAllById(ids);
+    }
+
+    /**
+     * Remove user rights on a file system object
+     * @param rightsUser user rights
+     */
+    private void deleteRightUserFileSystemObjectRedis(Iterable<RightUserFileSystemObjectRedis> rightsUser) {
+        rightUserFileSystemObjectRedisRepository.deleteAll(rightsUser);
     }
 
 }
