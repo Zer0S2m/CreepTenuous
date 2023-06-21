a6cd60ec543063bf29cc9421879dc1ebb5c67506
Aleksandr <ZeroDay9898@gmail.com>
2023-06-21 18:03:09 +0300

    Added tests for controller api rename file system object

---
+++ creep-tenuous-api/src/test/java/com/zer0s2m/creeptenuous/api/controllers/ControllerApiRenameFileSystemObjectTests.java
@@ -0,0 +1,71 @@
+package com.zer0s2m.creeptenuous.api.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zer0s2m.creeptenuous.common.data.DataRenameFileSystemObjectApi;
+import com.zer0s2m.creeptenuous.redis.models.DirectoryRedis;
+import com.zer0s2m.creeptenuous.redis.repository.DirectoryRedisRepository;
+import com.zer0s2m.creeptenuous.starter.test.annotations.TestTagControllerApi;
+import com.zer0s2m.creeptenuous.starter.test.helpers.UtilsAuthAction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestTagControllerApi
+public class ControllerApiRenameFileSystemObjectTests {
+
+    @Autowired
+    private DirectoryRedisRepository directoryRedisRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final String accessToken = UtilsAuthAction.builderHeader(UtilsAuthAction.generateAccessToken());
+
+    @Test
+    public void renameFileSystemObject_success() throws Exception {
+        DirectoryRedis directoryRedis = new DirectoryRedis(
+                UtilsAuthAction.LOGIN,
+                UtilsAuthAction.ROLE_USER,
+                "test",
+                "test_test",
+                Path.of("test").toString(), new ArrayList<>());
+        directoryRedisRepository.save(directoryRedis);
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.put("/api/v1/file-system-object/rename")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new DataRenameFileSystemObjectApi(
+                                "test_test",
+                                "new_test"
+
+                        )))
+                        .header("Authorization", accessToken))
+                .andExpect(status().isOk());
+
+        DirectoryRedis newDirectoryRedis = directoryRedisRepository.findById("test_test").get();
+
+        Assertions.assertEquals(newDirectoryRedis.getRealNameDirectory(), "new_test");
+
+        directoryRedisRepository.delete(directoryRedis);
+    }
+
+}
