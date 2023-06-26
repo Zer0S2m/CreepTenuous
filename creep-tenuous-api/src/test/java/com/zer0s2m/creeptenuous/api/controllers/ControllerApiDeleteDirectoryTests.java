cfa9e8e988410bf5f03480cab524d2f10bb60923
Aleksandr <ZeroDay9898@gmail.com>
2023-06-26 23:01:16 +0300

    Add tests for operation delete rights

--- creep-tenuous-api/src/test/java/com/zer0s2m/creeptenuous/api/controllers/ControllerApiDeleteDirectoryTests.java
+++ creep-tenuous-api/src/test/java/com/zer0s2m/creeptenuous/api/controllers/ControllerApiDeleteDirectoryTests.java
@@ -3,8 +3,11 @@ package com.zer0s2m.creeptenuous.api.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zer0s2m.creeptenuous.api.helpers.UtilsActionForFiles;
 import com.zer0s2m.creeptenuous.common.data.DataDeleteDirectoryApi;
+import com.zer0s2m.creeptenuous.common.enums.OperationRights;
 import com.zer0s2m.creeptenuous.redis.models.DirectoryRedis;
+import com.zer0s2m.creeptenuous.redis.models.RightUserFileSystemObjectRedis;
 import com.zer0s2m.creeptenuous.redis.repository.DirectoryRedisRepository;
+import com.zer0s2m.creeptenuous.redis.repository.RightUserFileSystemObjectRedisRepository;
 import com.zer0s2m.creeptenuous.services.system.core.ServiceBuildDirectoryPath;
 import com.zer0s2m.creeptenuous.starter.test.annotations.TestTagControllerApi;
 import com.zer0s2m.creeptenuous.starter.test.helpers.UtilsAuthAction;
@@ -49,24 +52,20 @@ public class ControllerApiDeleteDirectoryTests {
     @Autowired
     private DirectoryRedisRepository directoryRedisRepository;
 
+    @Autowired
+    private RightUserFileSystemObjectRedisRepository rightUserFileSystemObjectRedisRepository;
+
     private final String accessToken = UtilsAuthAction.builderHeader(UtilsAuthAction.generateAccessToken());
-
-    DataDeleteDirectoryApi RECORD_1 = new DataDeleteDirectoryApi(
-            new ArrayList<>(),
-            new ArrayList<>(),
-            "test_folder1",
-            "test_folder1"
-    );
-
-    DataDeleteDirectoryApi INVALID_RECORD = new DataDeleteDirectoryApi(
-            Arrays.asList("invalid", "path", "directory"),
-            Arrays.asList("invalid", "path", "directory"),
-            "test_folder1",
-            "test_folder1"
-    );
 
     @Test
     public void deleteDirectory_success() throws Exception {
+        DataDeleteDirectoryApi RECORD_1 = new DataDeleteDirectoryApi(
+                new ArrayList<>(),
+                new ArrayList<>(),
+                "test_folder1",
+                "test_folder1"
+        );
+
         Path newFolder = UtilsActionForFiles.preparePreliminaryFiles(
                 RECORD_1.systemDirectoryName(),
                 new ArrayList<>(),
@@ -79,7 +78,7 @@ public class ControllerApiDeleteDirectoryTests {
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(RECORD_1))
-                        .header("Authorization",  accessToken)
+                        .header("Authorization", accessToken)
                 )
                 .andExpect(status().isNoContent());
 
@@ -92,8 +91,13 @@ public class ControllerApiDeleteDirectoryTests {
                 MockMvcRequestBuilders.delete("/api/v1/directory/delete")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(INVALID_RECORD))
-                        .header("Authorization",  accessToken)
+                        .content(objectMapper.writeValueAsString(new DataDeleteDirectoryApi(
+                                Arrays.asList("invalid", "path", "directory"),
+                                Arrays.asList("invalid", "path", "directory"),
+                                "test_folder1",
+                                "test_folder1"
+                        )))
+                        .header("Authorization", accessToken)
                 )
                 .andExpect(status().isNotFound());
     }
@@ -104,7 +108,7 @@ public class ControllerApiDeleteDirectoryTests {
                 MockMvcRequestBuilders.delete("/api/v1/directory/delete")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization",  accessToken)
+                        .header("Authorization", accessToken)
                         .content(objectMapper.writeValueAsString(new DataDeleteDirectoryApi(
                                 new ArrayList<>(),
                                 new ArrayList<>(),
@@ -139,6 +143,43 @@ public class ControllerApiDeleteDirectoryTests {
                 .andExpect(status().isForbidden());
 
         directoryRedisRepository.delete(directoryRedis);
+    }
+
+    @Test
+    public void deleteDirectory_success_forbidden() throws Exception {
+        UtilsActionForFiles.createDirectories(
+                List.of("testDirectory"),
+                serviceBuildDirectoryPath,
+                logger);
+
+        DataDeleteDirectoryApi dataDeleteDirectoryApi = new DataDeleteDirectoryApi(
+                new ArrayList<>(), new ArrayList<>(), "testDirectory", "testDirectory");
+
+        DirectoryRedis directoryRedis = new DirectoryRedis(
+                "login",
+                UtilsAuthAction.ROLE_USER,
+                "testDirectory",
+                "testDirectory",
+                serviceBuildDirectoryPath.build(List.of("testDirectory")),
+                List.of(UtilsAuthAction.LOGIN));
+        RightUserFileSystemObjectRedis right = new RightUserFileSystemObjectRedis(
+                "testDirectory" + "__" + UtilsAuthAction.LOGIN, UtilsAuthAction.LOGIN,
+                List.of(OperationRights.SHOW, OperationRights.DELETE));
+
+        directoryRedisRepository.saveAll(List.of(directoryRedis));
+        rightUserFileSystemObjectRedisRepository.saveAll(List.of(right));
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/v1/directory/delete")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", accessToken)
+                        .content(objectMapper.writeValueAsString(dataDeleteDirectoryApi))
+                )
+                .andExpect(status().isNoContent());
+
+        directoryRedisRepository.deleteAll(List.of(directoryRedis));
+        rightUserFileSystemObjectRedisRepository.deleteAll(List.of(right));
     }
 
 }
