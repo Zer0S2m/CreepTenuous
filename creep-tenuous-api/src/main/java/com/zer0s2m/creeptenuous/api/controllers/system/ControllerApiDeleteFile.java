59dc2f1c643329ecd605b5cae2eb57768c03c7f0
Aleksandr <ZeroDay9898@gmail.com>
2023-10-29 20:54:35 +0300

    Atomic mode for file object deleting API handlers

--- creep-tenuous-api/src/main/java/com/zer0s2m/creeptenuous/api/controllers/system/ControllerApiDeleteFile.java
+++ creep-tenuous-api/src/main/java/com/zer0s2m/creeptenuous/api/controllers/system/ControllerApiDeleteFile.java
@@ -5,7 +5,14 @@ import com.zer0s2m.creeptenuous.common.annotations.V1APIRestController;
 import com.zer0s2m.creeptenuous.common.data.DataDeleteFileApi;
 import com.zer0s2m.creeptenuous.common.enums.OperationRights;
 import com.zer0s2m.creeptenuous.common.exceptions.FileObjectIsFrozenException;
+import com.zer0s2m.creeptenuous.common.exceptions.NoSuchFileExistsException;
+import com.zer0s2m.creeptenuous.core.atomic.annotations.AtomicFileSystem;
+import com.zer0s2m.creeptenuous.core.atomic.annotations.AtomicFileSystemExceptionHandler;
+import com.zer0s2m.creeptenuous.core.atomic.annotations.CoreServiceFileSystem;
+import com.zer0s2m.creeptenuous.core.atomic.context.ContextAtomicFileSystem;
 import com.zer0s2m.creeptenuous.core.atomic.handlers.AtomicSystemCallManager;
+import com.zer0s2m.creeptenuous.core.atomic.handlers.impl.ServiceFileSystemExceptionHandlerOperationDelete;
+import com.zer0s2m.creeptenuous.core.atomic.services.AtomicServiceFileSystem;
 import com.zer0s2m.creeptenuous.redis.events.FileRedisEventPublisher;
 import com.zer0s2m.creeptenuous.redis.services.security.ServiceManagerRights;
 import com.zer0s2m.creeptenuous.redis.services.system.ServiceDeleteFileRedis;
@@ -17,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.nio.file.Path;
@@ -36,6 +44,9 @@ public class ControllerApiDeleteFile implements ControllerApiDeleteFileDoc {
 
     private final FileRedisEventPublisher fileRedisEventPublisher;
 
+    private final AtomicFileSystemControllerApiDeleteFile atomicFileSystemControllerApiDeleteFile =
+            new AtomicFileSystemControllerApiDeleteFile();
+
     @Autowired
     public ControllerApiDeleteFile(
             ServiceDeleteFileRedis serviceDeleteFileRedis,
@@ -47,59 +58,91 @@ public class ControllerApiDeleteFile implements ControllerApiDeleteFileDoc {
     }
 
     /**
-     * Delete file
-     * <p>Called method via {@link AtomicSystemCallManager} - {@link ServiceDeleteFileImpl#delete(String, List)}
+     * Deleting a file. Supports atomic file system mode.
+     * <p>Called via {@link AtomicSystemCallManager} - {@link AtomicFileSystemControllerApiDeleteFile#delete(DataDeleteFileApi, String)}
      *
-     * @param file        file delete data
-     * @param accessToken raw JWT access token
+     * @param file        File delete data.
+     * @param accessToken Raw JWT access token.
      * @throws InvocationTargetException   Exception thrown by an invoked method or constructor.
      * @throws NoSuchMethodException       Thrown when a particular method cannot be found.
      * @throws InstantiationException      Thrown when an application tries to create an instance of a class
      *                                     using the newInstance method in class {@code Class}.
      * @throws IllegalAccessException      An IllegalAccessException is thrown when an application
-     *                                     tries to reflectively create an instance
-     * @throws FileObjectIsFrozenException file object is frozen
+     *                                     tries to reflectively create an instance.
+     * @throws FileObjectIsFrozenException File object is frozen.
+     * @throws IOException                 Signals that an I/O exception to some sort has occurred.
+     * @throws NoSuchFileExistsException   Exception raised when there is no file object.
      */
     @Override
     @DeleteMapping("/file/delete")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteFile(final @Valid @RequestBody @NotNull DataDeleteFileApi file,
                            @RequestHeader(name = "Authorization") String accessToken)
-            throws InvocationTargetException, NoSuchMethodException,
-            InstantiationException, IllegalAccessException, FileObjectIsFrozenException {
-        serviceManagerRights.setAccessClaims(accessToken);
-        serviceManagerRights.setIsWillBeCreated(false);
-
-        serviceDeleteFileRedis.setAccessToken(accessToken);
-        serviceDeleteFileRedis.setIsException(false);
-        boolean isRightsDirectory = serviceDeleteFileRedis.checkRights(file.systemParents());
-
-        if (!isRightsDirectory) {
-            serviceManagerRights.checkRightsByOperation(operationRightsDirectory, file.systemParents());
-
-            boolean isFrozen = serviceDeleteFileRedis.isFrozenFileSystemObject(file.systemParents());
-            if (isFrozen) {
-                throw new FileObjectIsFrozenException();
-            }
-        }
-
-        boolean isRightsFile = serviceDeleteFileRedis.checkRights(List.of(file.systemFileName()));
-        if (!isRightsFile) {
-            serviceManagerRights.checkRightsByOperation(operationRightsFile, file.systemFileName());
-
-            boolean isFrozen = serviceDeleteFileRedis.isFrozenFileSystemObject(file.systemFileName());
-            if (isFrozen) {
-                throw new FileObjectIsFrozenException();
-            }
-        }
-
-        Path source = AtomicSystemCallManager.call(
-                serviceDeleteFile,
-                file.systemFileName(),
-                file.systemParents()
+            throws InvocationTargetException, NoSuchMethodException, InstantiationException,
+            IllegalAccessException, FileObjectIsFrozenException, IOException, NoSuchFileExistsException {
+        AtomicSystemCallManager.call(
+                atomicFileSystemControllerApiDeleteFile,
+                file,
+                accessToken
         );
-        serviceDeleteFileRedis.delete(source, file.systemFileName());
-        fileRedisEventPublisher.publishDelete(file.systemFileName());
+    }
+
+    @CoreServiceFileSystem(method = "delete")
+    public final class AtomicFileSystemControllerApiDeleteFile implements AtomicServiceFileSystem {
+
+        /**
+         * Deleting a file.
+         *
+         * @param file        File delete data.
+         * @param accessToken Raw JWT access token.
+         * @throws FileObjectIsFrozenException File object is frozen.
+         * @throws NoSuchFileExistsException   Exception raised when there is no file object.
+         * @throws IOException                 Signals that an I/O exception to some sort has occurred.
+         */
+        @SuppressWarnings("unused")
+        @AtomicFileSystem(
+                name = "delete-file",
+                handlers = {
+                        @AtomicFileSystemExceptionHandler(
+                                isExceptionMulti = true,
+                                handler = ServiceFileSystemExceptionHandlerOperationDelete.class,
+                                operation = ContextAtomicFileSystem.Operations.DELETE
+                        )
+                }
+        )
+        public void delete(final @NotNull DataDeleteFileApi file, String accessToken)
+                throws FileObjectIsFrozenException, NoSuchFileExistsException, IOException {
+            serviceManagerRights.setAccessClaims(accessToken);
+            serviceManagerRights.setIsWillBeCreated(false);
+
+            serviceDeleteFileRedis.setAccessToken(accessToken);
+            serviceDeleteFileRedis.setIsException(false);
+            boolean isRightsDirectory = serviceDeleteFileRedis.checkRights(file.systemParents());
+
+            if (!isRightsDirectory) {
+                serviceManagerRights.checkRightsByOperation(operationRightsDirectory, file.systemParents());
+
+                boolean isFrozen = serviceDeleteFileRedis.isFrozenFileSystemObject(file.systemParents());
+                if (isFrozen) {
+                    throw new FileObjectIsFrozenException();
+                }
+            }
+
+            boolean isRightsFile = serviceDeleteFileRedis.checkRights(List.of(file.systemFileName()));
+            if (!isRightsFile) {
+                serviceManagerRights.checkRightsByOperation(operationRightsFile, file.systemFileName());
+
+                boolean isFrozen = serviceDeleteFileRedis.isFrozenFileSystemObject(file.systemFileName());
+                if (isFrozen) {
+                    throw new FileObjectIsFrozenException();
+                }
+            }
+
+            Path source = serviceDeleteFile.delete(file.systemFileName(), file.systemParents());
+            serviceDeleteFileRedis.delete(source, file.systemFileName());
+            fileRedisEventPublisher.publishDelete(file.systemFileName());
+        }
+
     }
 
 }
--- creep-tenuous-api/src/main/java/com/zer0s2m/creeptenuous/api/documentation/controllers/ControllerApiDeleteFileDoc.java
+++ creep-tenuous-api/src/main/java/com/zer0s2m/creeptenuous/api/documentation/controllers/ControllerApiDeleteFileDoc.java
@@ -2,6 +2,7 @@ package com.zer0s2m.creeptenuous.api.documentation.controllers;
 
 import com.zer0s2m.creeptenuous.common.data.DataDeleteFileApi;
 import com.zer0s2m.creeptenuous.common.exceptions.FileObjectIsFrozenException;
+import com.zer0s2m.creeptenuous.common.exceptions.NoSuchFileExistsException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -9,28 +10,31 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
 public interface ControllerApiDeleteFileDoc {
 
     /**
-     * Delete file
+     * Deleting a file. Supports atomic file system mode.
      *
-     * @param file        file delete data
-     * @param accessToken raw JWT access token
+     * @param file        File delete data.
+     * @param accessToken Raw JWT access token.
      * @throws InvocationTargetException   Exception thrown by an invoked method or constructor.
      * @throws NoSuchMethodException       Thrown when a particular method cannot be found.
      * @throws InstantiationException      Thrown when an application tries to create an instance of a class
      *                                     using the newInstance method in class {@code Class}.
      * @throws IllegalAccessException      An IllegalAccessException is thrown when an application
-     *                                     tries to reflectively create an instance
-     * @throws FileObjectIsFrozenException file object is frozen
+     *                                     tries to reflectively create an instance.
+     * @throws FileObjectIsFrozenException File object is frozen.
+     * @throws IOException                 Signals that an I/O exception to some sort has occurred.
+     * @throws NoSuchFileExistsException   Exception raised when there is no file object.
      */
     @Operation(
             method = "DELETE",
             summary = "Deleting a file",
             description = "Deleting a file",
-            tags = { "File" },
+            tags = {"File"},
             security = @SecurityRequirement(name = "Bearer Authentication"),
             requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
                     description = "Data to delete a file",
@@ -55,6 +59,7 @@ public interface ControllerApiDeleteFileDoc {
     void deleteFile(
             final DataDeleteFileApi file,
             @Parameter(hidden = true) String accessToken
-    ) throws InvocationTargetException, NoSuchMethodException,
-            InstantiationException, IllegalAccessException, FileObjectIsFrozenException;
+    ) throws InvocationTargetException, NoSuchMethodException, InstantiationException,
+            IllegalAccessException, FileObjectIsFrozenException, IOException, NoSuchFileExistsException;
+
 }
